f91cb8275ef22d3346684818992610a7595dd441
Muki <mukidev881@gmail.com>
2025-07-07 22:38:12 +0200

    show detail image

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -2,6 +2,7 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl,titleText){
 	'use strict';
@@ -32,6 +33,7 @@ function addThumbClickHandler(thumb){
 	thumb.addEventListener('click',function(eevnt){
 		event.preventDefault();
 		setDetailsFromThumb(thumb); 
+		showDetails()
 	})
 }
 
@@ -44,14 +46,21 @@ function getThumbnailArray(){
 
 function hideDetails(){
 	'use strict';
-	document.body.addClassList(HIDDEN_DETAIL_CLASS);
+	document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails(){
+	'use strict';
+	document.body.classList.remove(HIDDEN_DETAIL_CLASS);
 }
 
 function addKeyPressHandler(){
 	'use strict';
 	document.body.addEventListener('keyup',function(event){
 		event.preventDefault();
-		console.log(event.keyCode)
+		if(event.keyCode === ESC_KEY){
+			hideDetails();
+		}
 	})
 }
 
